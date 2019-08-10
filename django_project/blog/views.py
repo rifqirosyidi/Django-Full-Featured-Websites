f258f9a6bbe4f20ca81afce3c66c75db5372cf79
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-08-10 18:03:56 +0700

    styling and templating

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+dummy_posts = [
+    {
+        'author': 'Rifqi Rosyidi',
+        'title': 'Posted 1',
+        'content': 'Content Of Posted One By Rief',
+        'date_posted': '21 August, 2019',
+    },
+    {
+        'author': 'Neuron Planets',
+        'title': 'Posted 2',
+        'content': 'Content Of Posted Two By Neuron Planets',
+        'date_posted': '24 August, 2019',
+    },
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Hi Rief, Welcome to blog-home</h1>")
+    context = {
+        'posts': dummy_posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return  HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html', {'title': 'About Page'})
