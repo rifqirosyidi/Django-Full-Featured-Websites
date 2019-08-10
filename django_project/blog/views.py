c0cb96405744c137918a895bed2f91d99896d5c3
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-08-10 23:21:00 +0700

    add registration feature

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
-
-dummy_posts = [
-    {
-        'author': 'Rifqi Rosyidi',
-        'title': 'Posted 1',
-        'content': 'Content Of Posted One By Rief',
-        'date_posted': '21 August, 2019',
-    },
-    {
-        'author': 'Neuron Planets',
-        'title': 'Posted 2',
-        'content': 'Content Of Posted Two By Neuron Planets',
-        'date_posted': '24 August, 2019',
-    },
-]
 
 
 def home(request):
     context = {
-        'posts': dummy_posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
